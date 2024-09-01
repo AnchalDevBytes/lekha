ea745f8eb70a668871b601e58bcfd0e9e6788feb
anchalraj31082004 <anchalraj20045@gmail.com>
2024-09-01 12:43:20 +0530

    Setup middleware in frontend

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export default function middleware(request : NextRequest) {
+    const path = request.nextUrl.pathname;
+    const isPublicPath = path === "/" || path === "/publicRoutes/signup" || path === "/publicRoutes/signin" ;
+    const token = request.cookies.get('token')?.value ?? "";
+    
+    if(isPublicPath && token){
+        return NextResponse.redirect(new URL('/blogs', request.nextUrl))
+    }
+
+    if(!isPublicPath && !token) {
+        return NextResponse.redirect(new URL('/' , request.nextUrl))
+    }
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+        "/",
+        "/publicRoutes/signup",
+        "/publicRoutes/signin",
+        "/blogs",
+        "/blogs/create-blog"
+    ],
+};
